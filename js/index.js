b12d395ee606de516c84c0584d02034096a063e0
Pushp Bhadana <pushpbhadana@gmail.com>
2024-10-21 21:56:02 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,16 +14,6 @@ function myFunction(event) {
         document.getElementById("result").value += event.key; 
 } 
 
-var cal = document.getElementById("calcu"); 
-cal.onkeyup = function (event) { 
-    if (event.keyCode === 13) { 
-        console.log("Enter"); 
-        let x = document.getElementById("result").value 
-        console.log(x); 
-        solve(); 
-    } 
-} 
-
 // Function that evaluates the digit and return result 
 function solve() { 
     let x = document.getElementById("result").value 
